a2e70b07167517f73b5c4ca6db87c9fe9f75e336
ALirzq <fakhryalifahrizq@gmail.com>
2024-11-13 11:31:35 +0700

    Feat:[ALRZQ] Menghapus button Update di Departement

--- resources/views/departement.blade.php
+++ resources/views/departement.blade.php
@@ -3,12 +3,6 @@
 
     <div class="container mx-auto p-8">
         <!-- Header Section with Button -->
-        <div class="flex justify-between items-center mb-8">
-            <a href="#"
-                class="bg-gradient-to-r from-green-400 to-blue-500 hover:from-green-500 hover:to-blue-600 text-white font-semibold py-2 px-6 rounded-lg shadow-lg transition duration-300 transform hover:scale-105">
-                <i class="fas fa-plus mr-2"></i> Tambah Departemen
-            </a>
-        </div>
 
         <!-- Tabel Data Departemen -->
         <div class="overflow-hidden rounded-lg shadow-xl border border-gray-200 bg-gray-50">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,7 @@
     <section class="flex justify-center items-center min-h-screen bg-gray-100">
         <div class="flex flex-col items-center w-1/2 bg-white rounded-lg shadow-lg p-6">
             <h1 class="text-4xl font-bold text-blue-600 mb-4">Selamat Datang!</h1>
+            
         </div>
     </section>
 </x-layout>
